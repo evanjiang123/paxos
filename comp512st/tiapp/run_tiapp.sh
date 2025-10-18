5c570db80bb984c140a19ed7f36bf0576e23d5c8
Evan Jiang <evanjiangrd@gmail.com>
2025-10-18 16:10:07 -0400

    updated file path

--- comp512st/tiapp/run_tiapp.sh
+++ comp512st/tiapp/run_tiapp.sh
@@ -12,7 +12,7 @@ fi
 playernum=$1
 
 #TODO set this to where your code and jar file root dir is
-BASEDIR=$HOME/comp512/p2
+BASEDIR=$HOME/paxos/comp512st/tiapp
 
 #TODO update your group number here in place of XX
 group=26
